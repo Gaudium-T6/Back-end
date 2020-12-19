b22e0985d04636628d18d579a33cfcfd2e3c6ef3
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-12-18 19:25:42 -0600

    feat(swagger): swagger documentation config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { config } = require('./config/index');
 const gaudiumApi = require('./routes/index.js');
+const swaggerUi = require('swagger-ui-express');
+const apiDocumentation = require('./swagger.json');
 
 
 const app = express();
 
-// routes
+// documentation
+app.use('/', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
+// api routes
 gaudiumApi(app);
 
 //App endpoint
