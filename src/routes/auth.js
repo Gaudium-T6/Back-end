73efe670268c23cc7825578a86bc1863a22c077f
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-12-19 13:18:55 -0600

    feat(auth): auth module completed

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,35 +5,52 @@ const { UserModel } = require('../models/user');
 function authenticationApi(app) {
   app.use( session({secret: 'keyboard cat'}));
   app.post('/login', async function (req, res) {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      res.send('login failed');
+    const { email, password: pass } = req.body;
+    if (!email || !pass) {
+      res.json({
+        error: true,
+        message: 'Bad request'
+      });
     } else {
       const user = await UserModel.findOne({
         where: {
-          email: username,
-          password
+          email,
+          password: pass
         }
       });
 
-      console.log(user);
-
       if (!user) {
-        res.send('user not found');
+        res.json({
+          error: true,
+          message: 'User was not found'
+        });
       }
 
-      res.send('login success');
-      req.session.user = user;
-      // if(req.query.username === "jose" || req.query.password === "hunter2") {
-      // req.session.user = "";
-      // req.session.admin = true;
-      // req.session.admin = true;
-      // }
+      let { password, ...data} = user.dataValues;
+
+      const responseUser = {
+        name: `${data.name} ${data.last_name}`,
+        email: data.email,
+        discord_user: data.discord_user,
+        github_user: data.github_user
+      }
+
+      req.session.user = {
+        name: responseUser.name,
+        id: data.id
+      };
+
+      res.status(200).json({
+          data: responseUser,
+          message: 'User logged'
+      });
     }
   });
 
-  app.get('/logout', function (req) {
+  app.get('/logout', function (req, res) {
+    console.log(req.session);
     req.session.destroy();
+    res.send('logout successfully')
   });
 }
 
